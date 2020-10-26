f5189a225dd8f23487c608aab11e8a7f6676ace1
Maharshi Shah <maharshishah@mail.usf.edu>
2020-10-26 14:39:05 +0530

    Added Internet permission and identified listView programmatically. Initialized arrayAdapter for the listView

--- app/src/main/java/maharshi/myfirstapp/technewsreader/MainActivity.java
+++ app/src/main/java/maharshi/myfirstapp/technewsreader/MainActivity.java
@@ -3,12 +3,23 @@ package maharshi.myfirstapp.technewsreader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> articleTitles = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialized ListView along with arrayAdapter
+        ListView articlesListView = (ListView) findViewById(R.id.articlesListView);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, articleTitles);
+        articlesListView.setAdapter(arrayAdapter);
     }
 }
